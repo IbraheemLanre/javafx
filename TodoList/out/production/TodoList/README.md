8dadc2fa50e5d30fe31b910e4c43a5eb2b764cd9
IbraheemLanre <s2001108@edu.bc.fi>
2021-07-10 22:49:00 +0300

    displayed todo items descriptions, details and due dates

---
+++ TodoList/out/production/TodoList/README.md
@@ -0,0 +1,4 @@
+About the project - TODO LIST
+
+This is a todo list project that is created in Java using JavaFX as the UI
+
--- TodoList/src/com/ibraheem/todolist/Controller.java
+++ TodoList/src/com/ibraheem/todolist/Controller.java
@@ -1,6 +1,11 @@
 package com.ibraheem.todolist;
 
 import com.ibraheem.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.SelectionModel;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -11,11 +16,17 @@ public class Controller {
 
     private List<TodoItem> todoItems;
 
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
     public void initialize(){
         TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for Amina", LocalDate.of(2021, Month.SEPTEMBER, 14));
         TodoItem item2 = new TodoItem("Doctor's Appointment", "Get COVID vax at Jatkasaari", LocalDate.of(2021, Month.JULY, 30));
         TodoItem item3 = new TodoItem("Tax Office", "Visit tax office at Kluuvi on Monday", LocalDate.of(2021, Month.JULY, 12));
-        TodoItem item4 = new TodoItem("KEG email", "Reply to KEG email on PWA by Monday", LocalDate.of(2021, Month.JULY, 12));
+        TodoItem item4 = new TodoItem("KEG Email", "Reply to KEG email on PWA by Monday", LocalDate.of(2021, Month.JULY, 12));
         TodoItem item5 = new TodoItem("Send Money", "Send money for loan by Monday", LocalDate.of(2021, Month.SEPTEMBER, 12));
 
         todoItems = new ArrayList<TodoItem>();
@@ -25,5 +36,18 @@ public class Controller {
         todoItems.add(item4);
         todoItems.add(item5);
 
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView(){
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due Date: ");
+        sb.append(item.getDeadline().toString());
+        itemDetailsTextArea.setText(sb.toString());
     }
 }
