31cc70840eec643a996a1435aa2dce6f2308f53a
IbraheemLanre <s2001108@edu.bc.fi>
2021-07-10 14:24:07 +0300

    Added event and event handlers in JavaFx

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,14 +1,39 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField nameField;
-    
     @FXML
-    public void onButtonClick(){
-        System.out.println("Saying Hello..., " + nameField.getText());
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void init(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+
+    }
+
+    @FXML
+    public void onButtonClick(ActionEvent e){
+        if(e.getSource().equals(helloButton)){
+            System.out.println("Saying Hello..., " + nameField.getText());
+        }else if(e.getSource().equals(byeButton)){
+            System.out.println("Saying Bye..., " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
