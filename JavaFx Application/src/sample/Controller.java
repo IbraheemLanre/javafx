383e107c7ba8f19cdeb41ce05a9030b09bd10a4d
IbraheemLanre <s2001108@edu.bc.fi>
2021-07-18 17:20:33 +0300

    Added FileChooser, DirectChooser and Dialog boxes

--- JavaFx Application/src/sample/Controller.java
+++ JavaFx Application/src/sample/Controller.java
@@ -1,4 +1,66 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
+
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
+    public void initialize(){
+        button4.setEffect(new DropShadow());
+    }
+
+    @FXML
+    public void handleMouseEnter(){
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
+
+    @FXML
+    public void handleClick(){
+        FileChooser fileChooser = new FileChooser();
+        //fileChooser.setTitle("Save Application File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF","*.pdf"),
+                new FileChooser.ExtensionFilter("Image Files", "*'png", "*.jpg", "*.gif"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+                );
+        //DirectoryChooser directoryChooser = new DirectoryChooser();
+        List<File> file = fileChooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+        if(file != null){
+            for (int i=0; i <file.size(); i++){
+                System.out.println(file.get(i));
+            }
+//            System.out.println(file.getPath());
+        }else{
+            System.out.println("Chooser was cancelled");
+        }
+
+
+
+
+
+    }
 }
