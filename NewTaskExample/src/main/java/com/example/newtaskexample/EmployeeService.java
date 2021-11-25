1714d3f9bd8e8bea7e88f2e52e60ac9842b837ab
IbraheemLanre <s2001108@edu.bc.fi>
2021-11-26 00:53:51 +0200

    added data binding

---
+++ NewTaskExample/src/main/java/com/example/newtaskexample/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.newtaskexample;
+
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+
+public class EmployeeService extends Service<ObservableList<String>> {
+
+}
--- NewTaskExample/src/main/java/com/example/newtaskexample/HelloController.java
+++ NewTaskExample/src/main/java/com/example/newtaskexample/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.newtaskexample;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class HelloController {
     private Task<ObservableList<String>> task;
@@ -14,24 +14,38 @@ public class HelloController {
     @FXML
     private ListView listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
     public void initialize(){
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                ObservableList<String> employees = FXCollections.observableArrayList(
+                String [] names = {
                         "Ekeh John",
                         "Bill Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
                         "Bob Dylan"
-                );
+                };
+                ObservableList<String> employees = FXCollections.observableArrayList();
 
+                for (int i=0; i<6; i++){
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 6);
+                    Thread.sleep(200);
+                }
                 return employees;
             }
         };
 
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
