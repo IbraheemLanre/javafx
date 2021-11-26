31dec47ef16715049197354d3ab9d7c492dcef5d
IbraheemLanre <s2001108@edu.bc.fi>
2021-11-26 02:13:30 +0200

    simulating a bank account

---
+++ BankAccountThread/src/com/ibraheem/BankAccount.java
@@ -0,0 +1,32 @@
+package com.ibraheem;
+
+public class BankAccount {
+
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double initialBalance){
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount){
+        synchronized (this){
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount){
+        synchronized (this){
+            balance -= amount;
+        }
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void printAccountNumber(){
+        System.out.println("Account number = " + accountNumber);
+    }
+}
