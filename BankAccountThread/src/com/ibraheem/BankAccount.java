d7b45d7dc9a5a510a265aba232c906acdd4f1232
IbraheemLanre <s2001108@edu.bc.fi>
2021-12-01 19:49:58 +0200

    thread using tryLock method

--- BankAccountThread/src/com/ibraheem/BankAccount.java
+++ BankAccountThread/src/com/ibraheem/BankAccount.java
@@ -1,24 +1,51 @@
 package com.ibraheem;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
 
+    private Lock lock;
+
     public BankAccount(String accountNumber, double initialBalance){
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
     public void deposit(double amount){
-        synchronized (this){
-            balance += amount;
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance += amount;
+                }finally {
+                    lock.unlock();
+                }
+            }else{
+                System.out.println("Could not get the lock");
+            }
+        }catch (InterruptedException e){
+            //do something here
         }
     }
 
     public void withdraw(double amount){
-        synchronized (this){
-            balance -= amount;
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance -= amount;
+                }finally {
+                    lock.unlock();
+                }
+            }else {
+                System.out.println("Could not get the lock");
+            }
+        }catch (InterruptedException e){
+            // do something
         }
     }
 
