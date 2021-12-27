93578e4ef865fbffa21ef219eb4160aa0205bc76
IbraheemLanre <s2001108@edu.bc.fi>
2021-12-27 22:46:54 +0200

    added Lambda method reference

--- Strings/src/com/ibraheem/Main.java
+++ Strings/src/com/ibraheem/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> someBingONumbers = Arrays.asList(
+        List<String> someBingoNumbers = Arrays.asList(
                 "N40", "N36",
                 "B12", "B6",
                 "G53", "G40", "G60", "G50", "g64",
@@ -17,12 +17,16 @@ public class Main {
 
         List<String> gNumbers = new ArrayList<>();
 
-        someBingONumbers.forEach(number ->{
-            if(number.toUpperCase().startsWith("G")){
-                gNumbers.add(number);
-            }
-        });
-        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
-        gNumbers.forEach((String s) ->System.out.println(s));
+//        someBingoNumbers.forEach(number ->{
+//            if(number.toUpperCase().startsWith("G")){
+//                gNumbers.add(number);
+//            }
+//        });
+//        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+//        gNumbers.forEach((String s) ->System.out.println(s));
+        someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s->s.startsWith("G")).sorted()
+                .forEach(System.out::println);
     }
 }
